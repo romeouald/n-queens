e228649f6c66f131c6e00185097c07c6fcc20693
Romuald Szauer <hello@romuald.me>
2026-03-18 23:44:35 +0100

    Create project

---
+++ NQueens/NQueensApp.swift
@@ -0,0 +1,17 @@
+//
+//  NQueensApp.swift
+//  NQueens
+//
+//  Created by Romuald Szauer on 18/3/26.
+//
+
+import SwiftUI
+
+@main
+struct NQueensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Welcome to NQueens")
+        }
+    }
+}
---
+++ NQueensTests/NQueensTests.swift
@@ -0,0 +1,16 @@
+//
+//  NQueensTests.swift
+//  NQueensTests
+//
+//  Created by Romuald Szauer on 18/3/26.
+//
+
+import Testing
+
+struct NQueensTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
