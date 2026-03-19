63c203bda6cbd38c3a94ecb22f93e09de3556bae
Romuald Szauer <hello@romuald.me>
2026-03-19 01:49:56 +0100

    Add configuration view

---
+++ NQueens/UI/Configuration/ConfigurationView.swift
@@ -0,0 +1,55 @@
+//
+//  ConfigurationView.swift
+//  NQueens
+//
+//  Created by Romuald Szauer on 18/3/26.
+//
+
+import SwiftUI
+
+struct ConfigurationView: View {
+    @State var viewModel: ConfigurationViewModel
+    
+    var body: some View {
+        VStack {
+            Text("N Queens")
+                .font(.title.weight(.bold))
+
+            Form {
+                Section {
+                    Picker("Board size", selection: $viewModel.boardSize) {
+                        ForEach(4 ... 20, id: \.self) { i in
+                            Text("\(i)x\(i)").tag(i)
+                        }
+                    }
+                    .pickerStyle(.menu)
+                }
+                .listRowBackground(Color.overlayDark)
+                
+                Section {
+                    Button(action: {}) {
+                        Text("START GAME")
+                            .font(.default.weight(.heavy))
+                            .frame(maxWidth: .infinity)
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .controlSize(.large)
+                }
+                .listRowInsets(EdgeInsets())
+                .listRowBackground(Color.clear)
+            }
+            .scrollContentBackground(.hidden)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .padding()
+        .background(Color.background)
+        .foregroundStyle(Color.white)
+        .tint(Color.greenDark)
+    }
+}
+
+#Preview {
+    NavigationStack {
+        ConfigurationView(viewModel: .init())
+    }
+}
---
+++ NQueens/UI/Configuration/ConfigurationViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ConfigurationViewModel.swift
+//  NQueens
+//
+//  Created by Romuald Szauer on 18/3/26.
+//
+
+import Foundation
+
+struct ConfigurationViewModel {
+    var boardSize: Int = 4
+}
