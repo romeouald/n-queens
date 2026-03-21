7049471ace58cf84ec2fcbcc36d2cb4f2ad5b84f
Romuald Szauer <hello@romuald.me>
2026-03-21 02:09:30 +0100

    Display elapsed time

--- NQueens/UI/Scenes/Game/GameView.swift
+++ NQueens/UI/Scenes/Game/GameView.swift
@@ -11,15 +11,41 @@ struct GameView: View {
     @State var viewModel: GameViewModel
     
     var body: some View {
-        VStack {
+        VStack(spacing: 24) {
+            timerView
             BoardView(board: viewModel.board)
                 .onSquareTap { index in
                     viewModel.squareTapped(at: index)
                 }
+            
         }
+        .onAppear { viewModel.startGame() }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color.background)
         .tint(Color.greenDark)
+    }
+    
+    private var timerView: some View {
+        HStack(spacing: 8) {
+            Image(systemName: "clock")
+                .resizable()
+                .frame(width: 15, height: 15)
+                .fontWeight(.black)
+            TimelineView(.animation(minimumInterval: 0.05)) { _ in
+                Text(viewModel.elapsedTime.formatted(
+                    .time(pattern: .minuteSecond(
+                        padMinuteToLength: 2,
+                        fractionalSecondsLength: 1
+                    ))
+                ))
+            }
+        }
+        .foregroundStyle(.white)
+        .font(Font.system(size: 16, weight: .bold))
+        .monospacedDigit()
+        .padding(10)
+        .background(.black)
+        .clipShape(RoundedRectangle(cornerRadius: 5))
     }
 }
 
--- NQueens/UI/Scenes/Game/GameViewModel.swift
+++ NQueens/UI/Scenes/Game/GameViewModel.swift
@@ -12,12 +12,24 @@ class GameViewModel {
     var board: Chess.Board
     var game: any Chess.Game
     
+    var startTime: Date?
+    var elapsedTime: Duration {
+        guard let startTime else { return .zero }
+        let elapsed = -startTime.timeIntervalSinceNow
+        return Duration.seconds(elapsed)
+    }
+    
     init(
         boardSize: Int,
         game: any Chess.Game
     ) {
         self.board = .init(size: boardSize)
         self.game = game
+    }
+    
+    func startGame() {
+        guard startTime == nil else { return }
+        startTime = Date()
     }
     
     func squareTapped(at index: Int) {
